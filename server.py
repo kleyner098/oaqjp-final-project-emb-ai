0d34e4fef2bea3d7e342fb2fe45547dd3b1eec41
kleyner098 <kleyner_ecua@hotmail.com>
2026-07-13 14:44:31 -0400

    refactor server.py to 10 score pylint

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+
 from flask import render_template, request, Flask
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,7 +10,10 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
-
+    ''' This code receives the text from the HTML interface and 
+        runs emotion dectector over it using emotion_detector()
+        function. The output returned shows dictionary of emotions for the provided text.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
@@ -13,9 +21,13 @@ def sent_analyzer():
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
 
-    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
-
-    
+    return (
+        f"For the given statement, the system response is "
+        f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, 'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. "
+        f"The dominant emotion is {response['dominant_emotion']}."
+    )
 
 @app.route("/")
 def render_index_page():
@@ -25,4 +37,4 @@ def render_index_page():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
