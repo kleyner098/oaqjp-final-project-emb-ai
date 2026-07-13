dcc005387b92ab22dc8d4f4312df96b0bf140207
kleyner098 <kleyner_ecua@hotmail.com>
2026-07-13 13:57:56 -0400

    refactor emotion_anlyzer

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,17 +1,26 @@
-import requests 
+import requests
+import json
 
 def emotion_detector(text_to_analyze):
     # Define the URL for the sentiment analysis API
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 
     # Create the payload with the text to be analyzed
     myobj = { "raw_document": { "text": text_to_analyze } }
 
     # Set the headers with the required model ID for the API
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=myobj, headers=header)
 
-    # Return the label and score in a dictionary
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotion_reponse = formatted_response["emotionPredictions"][0]["emotion"]
+
+    dominant_emotion = max( emotion_reponse, key=emotion_reponse.get)
+
+    emotion_reponse['dominant_emotion'] = dominant_emotion
+
+    # Return a dictionary
+    return emotion_reponse
